6dd9e2f8675ffd55f3b81e6e64733f509f8e57c6
“Rafi030615” <rafiandhika@student.telkomuniversity.ac.id>
2025-02-26 17:10:16 +0700

    bismillah

--- testing.py
+++ testing.py
@@ -4,35 +4,19 @@ from urllib3.poolmanager import PoolManager
 import socket
 
 class SourceIPAdapter(HTTPAdapter):
-    def __init__(self, source_ip, **kwargs):
-        self.source_ip = source_ip
-        super().__init__(**kwargs)
+    def __init__(self, source_address, **kwargs):
+        self.source_address = source_address
+        super(SourceIPAdapter, self).__init__(**kwargs)
 
     def init_poolmanager(self, *args, **kwargs):
-        kwargs['socket_options'] = [(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(self.source_ip))]
-        kwargs['source_address'] = (self.source_ip, 0)
-        super().init_poolmanager(*args, **kwargs)
+        kwargs['source_address'] = (self.source_address, 0)
+        return super(SourceIPAdapter, self).init_poolmanager(*args, **kwargs)
 
-# 🔥 Bikin session yang maksa lewat uesimtun0 (IP 10.60.0.1)
 session = requests.Session()
 session.mount('http://', SourceIPAdapter('10.60.0.1'))
 session.mount('https://', SourceIPAdapter('10.60.0.1'))
 
-# 💀 Di fungsi lo tinggal ganti requests.get jadi session.get
-
-def fetch_content_size(url):
-    response = session.get(url)
-    return len(response.content)
-
-def make_request(url):
-    response = session.get(url)
-    return response.status_code
-
-# 🚀 Contoh pemakaian
-def main():
-    url = "http://testasp.vulnweb.com/"  # Ganti URL sesuai kebutuhan
-    print("Content size:", fetch_content_size(url))
-    print("Status code:", make_request(url))
-
-if __name__ == "__main__":
-    main()
+# 🔥 Request dipaksa lewat uesimtun0
+response = session.get('http://testasp.vulnweb.com/')
+print(f"Status Code: {response.status_code}")
+print(f"Content: {response.text[:500]}")  # Cetak 500 karakter pertama
